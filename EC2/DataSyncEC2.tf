420dd6f6d88f1700623dad03584da1c85a85d7d1
Giovanny Galindo <130039922+JesterCloud@users.noreply.github.com>
2024-02-21 14:39:51 +0100

    DataSync EC2

--- EC2/DataSyncEC2.tf
+++ EC2/DataSyncEC2.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "DataSync_EC2" {
   filter {
     name   = "image-id"
-    values = ["ami-073c509a30ffe8eaa"]
+    values = ["ami-123123123"] # Use a real DataSync AMI
   }
 }
 
@@ -13,8 +13,8 @@ resource "aws_iam_instance_profile" "DataS_Profile" {
 resource "aws_instance" "DataSync_Agent" {
   ami                         = data.aws_ami.DataSync_EC2.id
   instance_type               = "t3.xlarge"
-  vpc_security_group_ids      = ["sg-0785860525d735236"] #
-  subnet_id                   = "subnet-08426bd95ac6e76de" #
+  vpc_security_group_ids      = ["sg-0785860525d735236"] # Hard coded due to permissions
+  subnet_id                   = "subnet-08426bd95ac6e76de" # Hard coded due to permissions
   iam_instance_profile        = aws_iam_instance_profile.DataS_Profile.id
   associate_public_ip_address = true
 
