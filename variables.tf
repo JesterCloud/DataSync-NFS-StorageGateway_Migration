d6c8138e54eb3edb014749d5e5c04bba9fe4cbae
Giovanny Galindo <130039922+JesterCloud@users.noreply.github.com>
2024-02-21 14:24:29 +0100

    Variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 # Root/Variables -------
 
 variable "aws_region" {
-  default = "us-east-1" # Oregon Tiene mas AZ a, b, c.
+  default = "us-east-1" # Oregon has more AZ a, b, c, But for this we will be using East 1 since we only need 2
 }
